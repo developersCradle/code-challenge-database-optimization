8a8caeede359e9cf96c2fb07b135476332ab5499
Heikki Siikavirta <heikki.siikavirta@gmail.com>
2024-02-27 08:28:23 +0200

    added some test data to work with

--- database/database.sql
+++ database/database.sql
@@ -29,8 +29,38 @@ CREATE TABLE Recipients (
 -- We read query Recipients for calculations, so we index it for now.
 CREATE INDEX idx_receiver_id ON Recipients(receiver_id);
 
+
+-- Some data for Users
+
 INSERT INTO Users SET name = 'Some random name 1';
 INSERT INTO Users SET name = 'Some random name 2';
 INSERT INTO Users SET name = 'Some random name 3';
 INSERT INTO Users SET name = 'Some random name 4';
 INSERT INTO Users SET name = 'Some random name 5';
+
+-- Sample data for messages
+
+INSERT INTO Messages SET title = 'Hello', body = 'Hi there!', sender_id = 1;
+INSERT INTO Messages SET title = 'Meeting Tomorrow', body = 'Don\'t forget the meeting tomorrow at 10 AM.', sender_id = 1;
+INSERT INTO Messages SET title = 'Important Update', body = 'Please review the document attached for the latest update.', sender_id = 1;
+
+-- Same, but with some random timestamp
+
+INSERT INTO Messages SET title = 'Joku juttu', body = 'Fast. We need to speed up.', sent_at = '2020-02-25 14:30:00', sender_id = 2;
+INSERT INTO Messages SET title = 'Joku raportti', body = 'Please submit Invians test by Tuesday.', sent_at = '2020-02-24 09:45:00', sender_id = 3;
+
+
+-- Inserting recipients for sample messages
+
+INSERT INTO Recipients SET message_id = 1, receiver_id = 1;
+INSERT INTO Recipients SET message_id = 1, receiver_id = 2;
+INSERT INTO Recipients SET message_id = 1, receiver_id = 3;
+INSERT INTO Recipients SET message_id = 1, receiver_id = 4;
+INSERT INTO Recipients SET message_id = 1, receiver_id = 5; -- Message has a maximum of 5 recipients
+
+-- Inserting additional recipients, some random ones
+
+INSERT INTO Recipients SET message_id = 2, receiver_id = 3;
+INSERT INTO Recipients SET message_id = 3, receiver_id = 1;
+INSERT INTO Recipients SET message_id = 4, receiver_id = 2;
+INSERT INTO Recipients SET message_id = 5, receiver_id = 4;
